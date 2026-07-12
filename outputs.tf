3d30df729df7185e6b85e281789f7c99ff391028
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:29 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "consumption_budget_subscriptions" {
-  description = "All consumption_budget_subscription resources"
-  value       = azurerm_consumption_budget_subscription.consumption_budget_subscriptions
-}
 output "consumption_budget_subscriptions_amount" {
-  description = "List of amount values across all consumption_budget_subscriptions"
-  value       = [for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : v.amount]
+  description = "Map of amount values across all consumption_budget_subscriptions, keyed the same as var.consumption_budget_subscriptions"
+  value       = { for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : k => v.amount }
 }
 output "consumption_budget_subscriptions_etag" {
-  description = "List of etag values across all consumption_budget_subscriptions"
-  value       = [for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : v.etag]
+  description = "Map of etag values across all consumption_budget_subscriptions, keyed the same as var.consumption_budget_subscriptions"
+  value       = { for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : k => v.etag }
 }
 output "consumption_budget_subscriptions_filter" {
-  description = "List of filter values across all consumption_budget_subscriptions"
-  value       = [for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : v.filter]
+  description = "Map of filter values across all consumption_budget_subscriptions, keyed the same as var.consumption_budget_subscriptions"
+  value       = { for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : k => v.filter }
 }
 output "consumption_budget_subscriptions_name" {
-  description = "List of name values across all consumption_budget_subscriptions"
-  value       = [for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : v.name]
+  description = "Map of name values across all consumption_budget_subscriptions, keyed the same as var.consumption_budget_subscriptions"
+  value       = { for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : k => v.name }
 }
 output "consumption_budget_subscriptions_notification" {
-  description = "List of notification values across all consumption_budget_subscriptions"
-  value       = [for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : v.notification]
+  description = "Map of notification values across all consumption_budget_subscriptions, keyed the same as var.consumption_budget_subscriptions"
+  value       = { for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : k => v.notification }
 }
 output "consumption_budget_subscriptions_subscription_id" {
-  description = "List of subscription_id values across all consumption_budget_subscriptions"
-  value       = [for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : v.subscription_id]
+  description = "Map of subscription_id values across all consumption_budget_subscriptions, keyed the same as var.consumption_budget_subscriptions"
+  value       = { for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : k => v.subscription_id }
 }
 output "consumption_budget_subscriptions_time_grain" {
-  description = "List of time_grain values across all consumption_budget_subscriptions"
-  value       = [for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : v.time_grain]
+  description = "Map of time_grain values across all consumption_budget_subscriptions, keyed the same as var.consumption_budget_subscriptions"
+  value       = { for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : k => v.time_grain }
 }
 output "consumption_budget_subscriptions_time_period" {
-  description = "List of time_period values across all consumption_budget_subscriptions"
-  value       = [for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : v.time_period]
+  description = "Map of time_period values across all consumption_budget_subscriptions, keyed the same as var.consumption_budget_subscriptions"
+  value       = { for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : k => v.time_period }
 }
 
