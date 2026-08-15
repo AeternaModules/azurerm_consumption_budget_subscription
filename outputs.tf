3e677a4167a0e0b0f22287a73bff355e40526765
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:14:29 +0100

    Update to schema version v5.0.0

--- outputs.tf
+++ outputs.tf
@@ -12,7 +12,7 @@ output "consumption_budget_subscriptions_etag" {
 }
 output "consumption_budget_subscriptions_filter" {
   description = "Map of filter values across all consumption_budget_subscriptions, keyed the same as var.consumption_budget_subscriptions"
-  value       = { for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : k => v.filter if v.filter != null && length(v.filter) > 0 }
+  value       = { for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : k => one(v.filter) if v.filter != null && length(v.filter) > 0 }
 }
 output "consumption_budget_subscriptions_name" {
   description = "Map of name values across all consumption_budget_subscriptions, keyed the same as var.consumption_budget_subscriptions"
@@ -32,6 +32,6 @@ output "consumption_budget_subscriptions_time_grain" {
 }
 output "consumption_budget_subscriptions_time_period" {
   description = "Map of time_period values across all consumption_budget_subscriptions, keyed the same as var.consumption_budget_subscriptions"
-  value       = { for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : k => v.time_period if v.time_period != null && length(v.time_period) > 0 }
+  value       = { for k, v in azurerm_consumption_budget_subscription.consumption_budget_subscriptions : k => one(v.time_period) if v.time_period != null && length(v.time_period) > 0 }
 }
 
